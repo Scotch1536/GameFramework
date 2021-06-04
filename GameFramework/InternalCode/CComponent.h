a0dd88db3a0e0f50238b5cfddd754eafe60cc9e8
scotch1536 <beast5034@gmail.com>
2021-06-05 04:06:54 +0900

    インターフェース追加、コンポーネントクラス修正

--- GameFramework/InternalCode/CComponent.h
+++ GameFramework/InternalCode/CComponent.h
@@ -1,14 +1,18 @@
 #pragma once
+#include "CActor.h"
 
 class CComponent
 {
 private:
 	int mPriority;
+	IActorMethod& mOwnerMethod;
 public:
+	CComponent(CActor& owner);
+
+	virtual void Update() = 0;
+
 	const int& GetPriority()const
 	{
 		return mPriority;
 	}
-
-	virtual void Update() = 0;
 };
