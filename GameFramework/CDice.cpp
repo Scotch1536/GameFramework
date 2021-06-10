237c20a8dbe0af648aa4f30ae6a493c60d075d6f
Olivy <ryu8suke8@gmail.com>
2021-06-10 10:55:33 +0900

    ライトの初期化がActorでできることを確認

--- GameFramework/CDice.cpp
+++ GameFramework/CDice.cpp
@@ -11,42 +11,45 @@
 #include "InternalCode/CInputManager.h"
 #include "InternalCode/CModelDataManager.h"
 
-CDice::CDice(CLevel& owner):CActor(owner)
+CDice::CDice(CLevel& owner) :CActor(owner)
 {
-	CStaticMeshComponent* staticMesh = new CStaticMeshComponent(*this , CModelManager::GetInstance().GetModel("assets/dice/PlayerBox.x" , "assets/dice/"));
+	CStaticMeshComponent* staticMesh = new CStaticMeshComponent(*this, CModelManager::GetInstance().GetModel("assets/dice/PlayerBox.x", "assets/dice/"));
 	CCameraComponent* camera = new CCameraComponent(*this);
 	CLightComponent* light = new CLightComponent(*this);
 
-	DirectX::XMFLOAT3 eye(0 , 0 , -100);	//カメラ位置
-	DirectX::XMFLOAT3 lookat(0 , 0 , 0);	//注視点
-	DirectX::XMFLOAT3 up(0 , 1 , 0);		//カメラの上向きベクトル
+	DirectX::XMFLOAT3 eye(0, 0, -100);	//カメラ位置
+	DirectX::XMFLOAT3 lookat(0, 0, 0);	//注視点
+	DirectX::XMFLOAT3 up(0, 1, 0);		//カメラの上向きベクトル
 
 	camera->Init(
-		10.0f ,							//ニアクリップ
-		10000.0f ,						//ファークリップ
-		XM_PI / 4.0f ,					//視野角
-		Application::CLIENT_WIDTH ,		//スクリーン幅
-		Application::CLIENT_HEIGHT ,	//スクリーンの高さ
-		eye , lookat , up);
+		10.0f,							//ニアクリップ
+		10000.0f,						//ファークリップ
+		XM_PI / 4.0f,					//視野角
+		Application::CLIENT_WIDTH,		//スクリーン幅
+		Application::CLIENT_HEIGHT,	//スクリーンの高さ
+		eye, lookat, up);
 
-	CInputManager::GetInstance().AddAction("test" , { EButtonType::KEYBOARD,DIK_A } , std::bind(&CDice::Test , std::ref(*this)));
-	CInputManager::GetInstance().AddAction("XP" , { EButtonType::KEYBOARD,DIK_S } , std::bind(&CDice::Rot , std::ref(*this) , 0));
-	CInputManager::GetInstance().AddAction("XM" , { EButtonType::KEYBOARD,DIK_D } , std::bind(&CDice::Rot , std::ref(*this) , 1));
-	CInputManager::GetInstance().AddAction("YP" , { EButtonType::KEYBOARD,DIK_F } , std::bind(&CDice::Rot , std::ref(*this) , 2));
-	CInputManager::GetInstance().AddAction("YM" , { EButtonType::KEYBOARD,DIK_G } , std::bind(&CDice::Rot , std::ref(*this) , 3));
-	CInputManager::GetInstance().AddAction("ZP" , { EButtonType::KEYBOARD,DIK_H } , std::bind(&CDice::Rot , std::ref(*this) , 4));
-	CInputManager::GetInstance().AddAction("ZM" , { EButtonType::KEYBOARD,DIK_J } , std::bind(&CDice::Rot , std::ref(*this) , 5));
-	CInputManager::GetInstance().AddAction("Dst" , { EButtonType::KEYBOARD,DIK_L } , std::bind(&CDice::Destroy , std::ref(*this)));
+	light->Init(camera->GetEye(), XMFLOAT4(1, 1, -1, 0));
+	light->SetAmbient(XMFLOAT4(0.0f, 0.0f, 0.0f, 0.0f));
+
+	CInputManager::GetInstance().AddAction("test", { EButtonType::KEYBOARD,DIK_A }, std::bind(&CDice::Test, std::ref(*this)));
+	CInputManager::GetInstance().AddAction("XP", { EButtonType::KEYBOARD,DIK_S }, std::bind(&CDice::Rot, std::ref(*this), 0));
+	CInputManager::GetInstance().AddAction("XM", { EButtonType::KEYBOARD,DIK_D }, std::bind(&CDice::Rot, std::ref(*this), 1));
+	CInputManager::GetInstance().AddAction("YP", { EButtonType::KEYBOARD,DIK_F }, std::bind(&CDice::Rot, std::ref(*this), 2));
+	CInputManager::GetInstance().AddAction("YM", { EButtonType::KEYBOARD,DIK_G }, std::bind(&CDice::Rot, std::ref(*this), 3));
+	CInputManager::GetInstance().AddAction("ZP", { EButtonType::KEYBOARD,DIK_H }, std::bind(&CDice::Rot, std::ref(*this), 4));
+	CInputManager::GetInstance().AddAction("ZM", { EButtonType::KEYBOARD,DIK_J }, std::bind(&CDice::Rot, std::ref(*this), 5));
+	CInputManager::GetInstance().AddAction("Dst", { EButtonType::KEYBOARD,DIK_L }, std::bind(&CDice::Destroy, std::ref(*this)));
 }
 
 void CDice::Test()
 {
-	MessageBox(NULL , "Test" , "error" , MB_OK);
+	MessageBox(NULL, "Test", "error", MB_OK);
 }
 
 void CDice::Rot(int dire)
 {
-	switch(dire)
+	switch (dire)
 	{
 	case 0:
 		mTransform.rotation.angle.x++;
