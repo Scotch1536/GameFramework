e98b71419cd2180618340adaf1836b1f5fbba2b0
scotch1536 <beast5034@gmail.com>
2021-07-27 02:20:23 +0900

    背面カリング問題改善

--- GameFramework/GameFramework/Components/CPlaneMeshComponent.cpp
+++ GameFramework/GameFramework/Components/CPlaneMeshComponent.cpp
@@ -12,10 +12,10 @@ void CPlaneMeshComponent<SVertexColor>::CreateVertexData()
 {
 	mVertices.resize(4);
 
-	mVertices.at(0).Pos = { -0.5f,-0.5f,0.0f };
-	mVertices.at(1).Pos = { 0.5f,-0.5f,0.0f };
-	mVertices.at(2).Pos = { -0.5f,0.5f,0.0f };
-	mVertices.at(3).Pos = { 0.5f,0.5f,0.0f };
+	mVertices.at(0).Pos = { -0.5f,0.5f,0.0f };
+	mVertices.at(1).Pos = { 0.5f,0.5f,0.0f };
+	mVertices.at(2).Pos = { -0.5f,-0.5f,0.0f };
+	mVertices.at(3).Pos = { 0.5f,-0.5f,0.0f };
 
 	for(auto& vertex : mVertices)
 	{
@@ -32,10 +32,10 @@ void CPlaneMeshComponent<SVertexUV>::CreateVertexData()
 {
 	mVertices.resize(4);
 
-	mVertices.at(0).Pos = { -0.5f,-0.5f,0.0f };
-	mVertices.at(1).Pos = { 0.5f,-0.5f,0.0f };
-	mVertices.at(2).Pos = { -0.5f,0.5f,0.0f };
-	mVertices.at(3).Pos = { 0.5f,0.5f,0.0f };
+	mVertices.at(0).Pos = { -0.5f,0.5f,0.0f };
+	mVertices.at(1).Pos = { 0.5f,0.5f,0.0f };
+	mVertices.at(2).Pos = { -0.5f,-0.5f,0.0f };
+	mVertices.at(3).Pos = { 0.5f,-0.5f,0.0f };
 
 	mVertices.at(0).Tex = { 0.0f,0.0f };
 	mVertices.at(1).Tex = { 1.0f,0.0f };
